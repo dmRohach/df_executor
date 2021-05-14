f4a804e82f98314dc040e5398bc6a2422dfd62de
dmRohach <77681047+dmRohach@users.noreply.github.com>
2021-05-14 13:38:49 +0300

    Add files via upload

--- executors/__init__.py
+++ executors/__init__.py
@@ -1,22 +1,22 @@
 import subprocess
 
 
-def create_executor(human=None, inode=None):
-    if human:
-        command = HumanExecutor()
-    elif inode:
-        command = InodeExecutor()
-    else:
-        command = Executor()
-    return command
-
-
 class Executor:
     def __init__(self):
         self._terminal = 'df'
         self.keys = ['Filesystem', '1K-blocks', 'Used', 'Available', 'Use%', 'Mounted on']
         self.error = None
         self.result = None
+
+    @classmethod
+    def create_executor(cls, human=None, inode=None):
+        if human:
+            command = HumanExecutor()
+        elif inode:
+            command = InodeExecutor()
+        else:
+            command = Executor()
+        return command
 
     def make_result(self):
         _command = subprocess.Popen(self._terminal, stdout=subprocess.PIPE, stderr=subprocess.PIPE).communicate()
